a4d080ffa1a8a0114df3cdf6b1cb6c9ed6c7e0d3
Derya Ersöz <dryaersz@gmail.com>
2014-12-21 14:38:53 +0200

    Takvime ekleme işlemi yapıldı.

--- src/com/example/soft/EtkinlikAdapter.java
+++ src/com/example/soft/EtkinlikAdapter.java
@@ -1,20 +1,30 @@
 package com.example.soft;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.provider.CalendarContract;
+import android.provider.CalendarContract.Events;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class EtkinlikAdapter extends BaseAdapter{
 	
 	List<Etkinlik> liste_etkinlikler = new ArrayList<Etkinlik>();
 	private LayoutInflater mInflater;
+	static String stumu,etumu,starih,szaman,calName,calOrganizator,calLocation;
+    int  syil, say ,sgun, ssaat, sdakika,eyil,eay,egun,esaat,edakika;
+	String[] spart1,spart2,spart3,epart1,epart2,epart3;
 	
 	public EtkinlikAdapter(Context context, List<Etkinlik> liste_etkinlikler)
 	{
@@ -56,14 +66,106 @@ public class EtkinlikAdapter extends BaseAdapter{
 		
 		TextView tv_adi = (TextView) satirView.findViewById(R.id.adi);
 		
-		Etkinlik etkinlik = liste_etkinlikler.get(position);
+		final Etkinlik etkinlik = liste_etkinlikler.get(position);
 		
-		tv_tarih.setText(etkinlik.getDate_start());
+		tv_tarih.setText(etkinlik.getDate_start().split(" ")[0]);
 		
 		tv_tur.setText(etkinlik.getType());
 		
 		tv_adi.setText(etkinlik.getName());
+		
 
+		satirView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+                final Dialog dialog = new Dialog(MainActivity.icerik);
+                dialog.setContentView(R.layout.detay);
+                dialog.setTitle("Etkinlik Detayı");
+                
+                TextView title = (TextView) dialog.findViewById(R.id.textName);
+                title.setText("Adı: " + etkinlik.getName());
+                
+                TextView location = (TextView) dialog.findViewById(R.id.textLocation);
+                location.setText("Yer: " + etkinlik.getLocation());
+                
+                TextView startDate = (TextView) dialog.findViewById(R.id.textStartDate);
+                startDate.setText("Başlangıç Tarihi: " + etkinlik.getDate_start());
+                
+                TextView endDate = (TextView) dialog.findViewById(R.id.textEndDate);
+                endDate.setText("Bitiş Tarihi: " + etkinlik.getDate_end());
+                
+                TextView organizator = (TextView) dialog.findViewById(R.id.textOrganizator);
+                organizator.setText("Düzenleyen: " + etkinlik.getOrganizer());
+                
+                Button declineButton = (Button) dialog.findViewById(R.id.closeButton);
+                declineButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                
+                Button btn_add = (Button) dialog.findViewById(R.id.addButton);
+                btn_add.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+	                    	Calendar cal = Calendar.getInstance();     
+	                        Intent intent = new Intent(Intent.ACTION_INSERT);
+	                        intent.setType("vnd.android.cursor.item/event");
+	                        
+	                        intent.putExtra("allDay", false);
+	                        intent.putExtra("rrule", "FREQ=YEARLY");
+	                        intent.putExtra("title", etkinlik.getName());
+	                        intent.putExtra("description", "");
+	                        intent.putExtra(Events.EVENT_LOCATION, etkinlik.getLocation());
+	
+	                        String str_bas_tarih = etkinlik.getDate_start();
+	                        str_bas_tarih = str_bas_tarih.replace(".", ":");
+	                        String str_dizi_tumu[] = str_bas_tarih.split(" ");
+	                        String str_dizi_tarih[] = str_dizi_tumu[0].split(":");
+	                        String str_dizi_saat[] = str_dizi_tumu[1].split(":");
+	                        
+	                        Calendar beginTime = Calendar.getInstance();
+	                    	beginTime.set(Integer.valueOf(str_dizi_tarih[2]),
+	                    			Integer.valueOf(str_dizi_tarih[1])-1, 
+	                    			Integer.valueOf(str_dizi_tarih[0]), 
+	                    			Integer.valueOf(str_dizi_saat[0]), 
+	                    			Integer.valueOf(str_dizi_saat[1]));
+	                    	
+	                    	intent.putExtra("beginTime", beginTime.getTimeInMillis());
+	                    	
+	                    	
+	                        
+	                        String str_bit_tarih = etkinlik.getDate_end();
+	                        str_bit_tarih = str_bit_tarih.replace(".", ":");
+	                        String str_bit_dizi_tumu[] = str_bit_tarih.split(" ");
+	                        String str_bit_dizi_tarih[] = str_bit_dizi_tumu[0].split(":");
+	                        String str_bit_dizi_saat[] = str_bit_dizi_tumu[1].split(":");
+
+	                    	Calendar end = Calendar.getInstance();
+	                    	end.set(Integer.valueOf(str_bit_dizi_tarih[2]),
+	                    			Integer.valueOf(str_bit_dizi_tarih[1])-1, 
+	                    			Integer.valueOf(str_bit_dizi_tarih[0]), 
+	                    			Integer.valueOf(str_bit_dizi_saat[0]), 
+	                    			Integer.valueOf(str_bit_dizi_saat[1]));
+
+	                    	intent.putExtra("endTime", end.getTimeInMillis());
+
+	                        MainActivity.icerik.startActivity(intent);
+
+                    }
+                });
+                
+                
+            	dialog.show();
+                
+                
+				
+			}
+		});
+		
 		return satirView;
 	}
 
--- src/com/example/soft/MainActivity.java
+++ src/com/example/soft/MainActivity.java
@@ -5,6 +5,7 @@ import com.example.soft.MainActivity;
 import com.example.soft.R;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,17 +27,15 @@ import android.widget.Toast;
  
 public class MainActivity extends Activity 
 {
-	private static final String WebserviceClient = null;
+	
+	public static Context icerik;
 	TextView myCalendarDate,location,title,organizator,eDate,sDate;
 	ImageButton imgDate; 
 	
-	static String stumu,etumu,starih,szaman,calName,calOrganizator,calLocation;
-    int  syil, say ,sgun, ssaat, sdakika,eyil,eay,egun,esaat,edakika;
-	String[] spart1,spart2,spart3,epart1,epart2,epart3;
 	
    	private Calendar myCalendar = Calendar.getInstance();
    	SimpleDateFormat fmtDateAndTime =new SimpleDateFormat("yyyy-MM-dd");
-   	//String formatted =fmtDateAndTime.format(myCalender.getTime());
+
    	
    	DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
          	public void onDateSet(DatePicker view, int year, int monthOfYear,
@@ -64,7 +63,7 @@ public class MainActivity extends Activity
                super.onCreate(savedInstanceState);
                setContentView(R.layout.activity_main);
               
-               
+               icerik = MainActivity.this;
                final ListView lv = (ListView) findViewById(R.id.listView1);
                WebService etkinlikleri_getir = new WebService(getApplicationContext(), lv);
            	   etkinlikleri_getir.execute();
@@ -160,246 +159,10 @@ public class MainActivity extends Activity
 					}
 				}
 			});
-               //Generate list View from ArrayList
-              // displayListView();
- 
-               //checkButtonClick();
  
             }
             
-            /*
-            
-            private void displayListView()
-            {
-             	
-                    //Array list of countries
-                    ArrayList<States> stateList = new ArrayList<States>();
- 
-                    States _states = new States("Tiyatro","Tiyatro yeri içeriği",false);
-                    stateList.add(_states);
-                    _states = new States("Sinema","Sınema İceriği",true);
-                    stateList.add(_states);
-                    _states = new States("Kurlar","Goa",false);
-                    stateList.add(_states);
-                    _states = new States("Kongre","Kongre İçerigi",true);
-                    stateList.add(_states);
-                    _states = new States("Panel","Panel İçerigi",true);
-                    stateList.add(_states);
-                    _states = new States("Seminer","Seminer  İçerigi",false);
-                    stateList.add(_states);
-                    _states = new States("Şenlik","Senlik İçeriği",false);
-                    stateList.add(_states);
-                    _states = new States("Yarışmalar","West Bengal",false);
-                    stateList.add(_states);
- 
-                    //create an ArrayAdaptar from the String Array
-                    dataAdapter = new MyCustomAdapter(this,R.layout.state_info, stateList);
-                    ListView listView = (ListView) findViewById(R.id.listView1);
-                    // Assign adapter to ListView
-                    listView.setAdapter(dataAdapter);
-                   
- 
-                    listView.setOnItemClickListener(new OnItemClickListener() 
-                    {
- 
-                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) 
-                            {
-                            	
-                                     //When clicked, show a toast with the TextView text
-                                    States state = (States) parent.getItemAtPosition(position);
-                                    Toast.makeText(getApplicationContext(),"Clicked on Row: " + state.getName(), 
-                                   Toast.LENGTH_LONG).show();
-                           
-          
-                            }
-                    });
-            }
-         
- */
- /*           
-private class MyCustomAdapter extends ArrayAdapter<States>
-{
- 
-   private ArrayList<States> stateList;
- 
-  public MyCustomAdapter(Context context, int textViewResourceId, 
- 
-  ArrayList<States> stateList) 
-  {
-        super(context, textViewResourceId, stateList);
-        this.stateList = new ArrayList<States>();
-        this.stateList.addAll(stateList);
-  }
- 
-    private class ViewHolder
-    {
-      TextView code;
-      CheckBox name;
-      
-    }
- 
-   
-    @Override
-   public View getView(final int position, View convertView, ViewGroup parent) 
-    {
 
-            ViewHolder holder = null;
- 
-            Log.v("ConvertView", String.valueOf(position));
- 
-            if (convertView == null)
-            {
- 
-               LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
- 
-               convertView = vi.inflate(R.layout.state_info, null);
- 
-              holder = new ViewHolder();
-              holder.code = (TextView) convertView.findViewById(R.id.tur);
-              holder.name = (CheckBox) convertView.findViewById(R.id.cb);
-             
-             TextView tarih = (TextView) convertView.findViewById(R.id.tarih);  
-             
-              convertView.setTag(holder);
- 
-                        holder.name.setOnClickListener( new View.OnClickListener() 
-                        {
-                                   public void onClick(View v)  
-                                   {
-                                     CheckBox cb = (CheckBox) v;
-                                     States _state = (States) cb.getTag();
- 
-                                    
- 
-                                  if(  cb.isChecked()==true){
-                                	  _state.setSelected(cb.isChecked());
-                                    showDialog(v);
-                                    Intent calIntent = new Intent(Intent.ACTION_INSERT);
-                                    calIntent.setData(CalendarContract.Events.CONTENT_URI);
-                                    calIntent.putExtra(Events.TITLE, calName);
-                                    calIntent.putExtra(Events.EVENT_LOCATION, calLocation);
-                                    calIntent.putExtra(Events.DESCRIPTION, "garip ");
-                                   // Toast.makeText(getApplicationContext(), "yıl : "+syil+" ay: "+say, Toast.LENGTH_SHORT).show();
-                                    Calendar startTime = Calendar.getInstance();
-                                    startTime.set(syil, say, sgun, ssaat, sdakika);
-                                 
-                                    
-                                    Calendar endTime = Calendar.getInstance();
-                                    endTime.set(eyil,eay,egun,esaat,edakika);
-                                    calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
-                                    startTime.getTimeInMillis());
-                                    Toast.makeText(getApplicationContext(), "yıl : "+syil+" ay: "+say, Toast.LENGTH_SHORT).show();
-                                    calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
-                                    endTime.getTimeInMillis());
-                                    startActivity(calIntent);
-                                    //bak askım tarihi alıp yılını burasonra gunu saat ve dk alıcaksın ok
-                                    //ya
-                                    //sonra ayını 
-
-                                  }
-                                  else{
-                                	  _state.setSelected(cb.isChecked());
-                                  }
-                                          
-                                 }
-                        	
-							
-								
-                        });
- 
-            }
-            
-            else
-            {
-                holder = (ViewHolder) convertView.getTag();
-            }
- 
-            States state = stateList.get(position);
- 
-            holder.code.setText(" (" + state.getCode() + ")");
-            holder.name.setText(state.getName());
-            holder.name.setChecked(state.isSelected());
- 
-            holder.name.setTag(state);
-		
-            convertView.setOnClickListener(new OnClickListener(){
-            	public void onClick(View arg0){
-            		
-            		 // Create custom dialog object
-	                final Dialog dialog = new Dialog(MainActivity.this);
-	                // Include dialog.xml file
-	                dialog.setContentView(R.layout.detay);
-	                // Set dialog title
-	                dialog.setTitle("Etkinlik Detayı");
-	               
-	 
-	                // set values for custom dialog components - text, image and button
-	                TextView title = (TextView) dialog.findViewById(R.id.name_txt);
-                    calName  =title.getText().toString();
-	                TextView organizator = (TextView) dialog.findViewById(R.id.organiz_txt);
-	                calOrganizator  =organizator.getText().toString();
-	                TextView location = (TextView) dialog.findViewById(R.id.location_txt);
-	                calLocation= location.getText().toString();
-	                TextView eDate = (TextView) dialog.findViewById(R.id.enddate_txt);
-	               
-	                etumu=eDate.getText().toString();
-	              
-	                epart1 = etumu.split(" ");
-	            	String etarih = epart1[0]; // 004
-	            	String ezaman = epart1[1];
-	            	 Toast.makeText(getApplicationContext(), ezaman, Toast.LENGTH_SHORT).show();
-	            	epart2=etarih.split("-");
-	            	 eyil=Integer.parseInt(epart2[0]);
-	            	 eay=Integer.parseInt(epart2[1]);
-	            	 egun=Integer.parseInt(epart2[2]);
-	            	epart3=ezaman.split(":");
-	            	 esaat=Integer.parseInt(epart3[0]);
-	            	 edakika=Integer.parseInt(epart3[1]);
-	                
-	                TextView sDate = (TextView) dialog.findViewById(R.id.startdate_txt);
-	                stumu=sDate.getText().toString();
-	               
-	                spart1 = stumu.split(" ");
-	            	String starih = spart1[0]; // 004
-	            	String szaman = spart1[1];
-	            	spart2=starih.split("-");
-	            	 syil=Integer.parseInt(spart2[0]);
-	            	 say=Integer.parseInt(spart2[1]);
-	            	 sgun=Integer.parseInt(spart2[2]);
-	            	spart3=szaman.split(":");
-	            	 ssaat=Integer.parseInt(spart3[0]);
-	            	 sdakika=Integer.parseInt(spart3[1]);
-	            
-	              //  Toast.makeText(getApplicationContext(), "tarih : "+starih+" zaman: "+szaman, Toast.LENGTH_SHORT).show();
-	              //  Toast.makeText(getApplicationContext(), "yıl : "+syil+" ay: "+say, Toast.LENGTH_SHORT).show();
-	               // Toast.makeText(getApplicationContext(), "saat : "+ssaat+" dak: "+sdakika, Toast.LENGTH_SHORT).show();
-
-	                
-	             
-	 
-	                dialog.show();
-	                 
-	                Button declineButton = (Button) dialog.findViewById(R.id.closeButton);
-	                // if decline button is clicked, close the custom dialog
-	                declineButton.setOnClickListener(new OnClickListener() {
-	                    @Override
-	                    public void onClick(View v) {
-	                        // Close dialog
-	                        dialog.dismiss();
-	                    }
-	                });
-				}
-			
-            
-            
-            
-    });
- 
-            return convertView;
-    }
- 
-}*/
-    
 public void showDialog(View v)
 {
 	
